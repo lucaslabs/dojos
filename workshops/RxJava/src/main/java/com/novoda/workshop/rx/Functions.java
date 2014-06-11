c15ec052863b3c33d6978f10ba560b780cbbdc07
Daniele Bonaldo <dany.bony@gmail.com>
2014-06-11 18:41:47 +0100

    Missing just the last TODO of the first stage

--- workshops/RxJava/src/main/java/com/novoda/workshop/rx/Functions.java
+++ workshops/RxJava/src/main/java/com/novoda/workshop/rx/Functions.java
@@ -1,5 +1,6 @@
 package com.novoda.workshop.rx;
 
+import rx.Observable;
 import rx.functions.Func1;
 
 public class Functions {
@@ -15,5 +16,71 @@ public class Functions {
 
     private static boolean isEven(Integer integer) {
         return (integer % 2) == 0;
+    }
+
+    public static Func1<Integer, Boolean> isOdd() {
+        return new Func1<Integer, Boolean>() {
+            @Override
+            public Boolean call(Integer integer) {
+                return !isEven(integer);
+            }
+        };
+    }
+
+    public static Func1<Integer, Integer> doubleInt() {
+        return new Func1<Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer) {
+                return doubleInt(integer);
+            }
+        };
+    }
+
+    private static Integer doubleInt(Integer integer) {
+        return integer * 2;
+    }
+
+    public static Func1<Integer, String> prependIntegerString() {
+        return new Func1<Integer, String>() {
+            @Override
+            public String call(Integer integer) {
+                return "Integer : " + integer;
+            }
+        };
+    }
+
+    public static Func1<Integer, Observable<Integer>> repeatThreeTimes() {
+        return new Func1<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> call(Integer integer) {
+                    return Observable.from(integer).repeat(3);
+            }
+        };
+    }
+
+    public static Func1<Integer, Observable<Integer>> filterAndRepeatIfEven() {
+        return new Func1<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> call(Integer integer) {
+                if (isEven(integer)) {
+                    return Observable.from(integer).repeat(3);
+                } else {
+                    return Observable.from(integer);
+                }
+            }
+        };
+    }
+
+    public static Func1<Integer, Observable<Integer>> evenOrError() {
+        return new Func1<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> call(Integer integer) {
+                if (isEven(integer)) {
+                    return Observable.from(integer);
+                } else {
+                    return Observable.error(new IllegalArgumentException("NO odd elements señor..."));
+                }
+            }
+        };
     }
 }
--- workshops/RxJava/src/main/java/com/novoda/workshop/rx/RxJavaBasics.java
+++ workshops/RxJava/src/main/java/com/novoda/workshop/rx/RxJavaBasics.java
@@ -1,28 +1,23 @@
 package com.novoda.workshop.rx;
 
 import com.novoda.workshop.rx.observer.IntegerPrinterObserver;
+import com.novoda.workshop.rx.observer.StringPrinterObserver;
 
 import java.util.Arrays;
 import java.util.List;
 
 import rx.Observable;
 
+import static com.novoda.workshop.rx.Functions.doubleInt;
+import static com.novoda.workshop.rx.Functions.evenOrError;
+import static com.novoda.workshop.rx.Functions.filterAndRepeatIfEven;
 import static com.novoda.workshop.rx.Functions.isEven;
+import static com.novoda.workshop.rx.Functions.prependIntegerString;
+import static com.novoda.workshop.rx.Functions.repeatThreeTimes;
 
 public class RxJavaBasics {
 
     private static final List<Integer> INTEGERS = Arrays.asList(0, 1, 2, 3, 4, 5, 6);
-
-    /*
-        TODO:
-        - Multiply all elements by 2
-        - Get only even element and multiply them by 2
-        - Prepend the string "Integer : " in front of every element
-        - Repeat 3 time every element
-        - Repeat 3 times every even element only say once odd ones
-        - Fail if an element is not even.
-        - If an element is not even restart and double the entire sequence
-     */
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -31,6 +26,44 @@ public class RxJavaBasics {
                 .filter(isEven())
                 .subscribe(new IntegerPrinterObserver());
 
+        printExerciseTitle("Multiply all elements by 2");
+        Observable.from(INTEGERS)
+                .map(doubleInt())
+                .subscribe(new IntegerPrinterObserver());
+
+        printExerciseTitle("Get only even element and multiply them by 2");
+        Observable.from(INTEGERS)
+                .filter(isEven())
+                .map(doubleInt())
+                .subscribe(new IntegerPrinterObserver());
+
+        printExerciseTitle("Prepend the string \"Integer : \" in front of every element");
+        Observable.from(INTEGERS)
+                .map(prependIntegerString())
+                .subscribe(new StringPrinterObserver());
+
+        printExerciseTitle("Repeat 3 time every element");
+        Observable.from(INTEGERS)
+                .switchMap(repeatThreeTimes())
+                .subscribe(new IntegerPrinterObserver());
+
+        printExerciseTitle("Repeat 3 times every even element only say once odd ones");
+        Observable.from(INTEGERS)
+                .switchMap(filterAndRepeatIfEven())
+                .subscribe(new IntegerPrinterObserver());
+
+        printExerciseTitle("Fail if an element is not even");
+        Observable.from(INTEGERS)
+                .flatMap(evenOrError())
+                .subscribe(new IntegerPrinterObserver());
+
+        printExerciseTitle("If an element is not even restart and double the entire sequence");
+        // TODO
+
     }
 
+    private static void printExerciseTitle(String title) {
+        System.out.println();
+        System.out.println(title);
+    }
 }
