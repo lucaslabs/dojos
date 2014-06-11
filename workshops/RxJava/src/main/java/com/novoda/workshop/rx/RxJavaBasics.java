a6a6800e5acc46389036655648b89a2d449e8c44
Blundell <blundell.app.dev@gmail.com>
2014-06-11 21:06:18 +0100

    Euston we have Stage 1 ignition

--- workshops/RxJava/src/main/java/com/novoda/workshop/rx/RxJavaBasics.java
+++ workshops/RxJava/src/main/java/com/novoda/workshop/rx/RxJavaBasics.java
@@ -1,11 +1,15 @@
 package com.novoda.workshop.rx;
 
 import com.novoda.workshop.rx.observer.IntegerPrinterObserver;
+import com.novoda.workshop.rx.observer.StringPrinterObserver;
 
+import java.security.InvalidAlgorithmParameterException;
 import java.util.Arrays;
 import java.util.List;
 
 import rx.Observable;
+import rx.exceptions.OnErrorThrowable;
+import rx.functions.Func1;
 
 import static com.novoda.workshop.rx.Functions.isEven;
 
@@ -25,12 +29,103 @@ public class RxJavaBasics {
      */
 
     public static void main(String[] args) throws InterruptedException {
-
         System.out.println("Observable.from(INTEGERS).filter(isEven()).subscribe(new IntegerPrinterObserver());");
-        Observable.from(INTEGERS)
-                .filter(isEven())
+//        multiplyByTwo();
+//        getOnlyEvenElementsAndMultiplyByTwo();
+//        prependIntegerWord();
+//        repeatThreeTimes();
+//        repeatEven3TimesOddOnce();
+        final Observable<Integer> from = Observable.from(INTEGERS);
+        from
+                .flatMap(onEvenThrowError())
+                .onErrorFlatMap(new Func1<OnErrorThrowable, Observable<? extends Integer>>() {
+                    @Override
+                    public Observable<? extends Integer> call(OnErrorThrowable onErrorThrowable) {
+                        return Observable.from(INTEGERS).map(new Func1<Integer, Integer>() {
+                            @Override
+                            public Integer call(Integer integer) {
+                                return integer * 2;
+                            }
+                        });
+                    }
+                })
                 .subscribe(new IntegerPrinterObserver());
-
     }
 
+    private static Func1<Integer, Observable<Integer>> onEvenThrowError() {
+        return new Func1<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> call(Integer integer) {
+                if (integer % 2 != 0)
+                    return Observable.error(new InvalidAlgorithmParameterException());
+                else
+                    return Observable.just(integer);
+            }
+        };
+    }
+
+    private static void repeatEven3TimesOddOnce() {
+        Observable.from(INTEGERS)
+                .flatMap(new Func1<Integer, Observable<Integer>>() {
+                    @Override
+                    public Observable<Integer> call(Integer integer) {
+                        if (integer % 2 == 0)
+                            return Observable.just(integer).repeat(3);
+                        else
+                            return Observable.just(integer);
+                    }
+                })
+                .subscribe(new IntegerPrinterObserver());
+    }
+
+    private static void repeatThreeTimes() {
+        Observable.from(INTEGERS)
+                .flatMap(new Func1<Integer, Observable<Integer>>() {
+                    @Override
+                    public Observable<Integer> call(Integer integer) {
+                        return Observable.from(integer, integer, integer);
+                    }
+                })
+                .subscribe(new IntegerPrinterObserver());
+    }
+
+    private static void prependIntegerWord() {
+        Observable.from(INTEGERS)
+                .map(new Func1<Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer integer) {
+                        return integer * 2;
+                    }
+                })
+                .map(new Func1<Integer, String>() {
+                    @Override
+                    public String call(Integer integer) {
+                        return "Integer : " + integer;
+                    }
+                })
+                .subscribe(new StringPrinterObserver());
+    }
+
+    private static void getOnlyEvenElementsAndMultiplyByTwo() {
+        Observable.from(INTEGERS)
+                .filter(isEven())
+                .map(new Func1<Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer integer) {
+                        return integer * 2;
+                    }
+                })
+                .subscribe(new IntegerPrinterObserver());
+    }
+
+    private static void multiplyByTwo() {
+        Observable.from(INTEGERS)
+                .map(new Func1<Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer integer) {
+                        return integer * 2;
+                    }
+                })
+                .subscribe(new IntegerPrinterObserver());
+    }
 }
