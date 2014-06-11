f015e3c23da9e2eef1083452f2b5ffb6a76b51a3
pmarois <pmarois@genymobile.com>
2014-06-11 18:42:15 +0100

    first stage uncomplete

--- workshops/RxJava/src/main/java/com/novoda/workshop/rx/Functions.java
+++ workshops/RxJava/src/main/java/com/novoda/workshop/rx/Functions.java
@@ -1,7 +1,7 @@
 package com.novoda.workshop.rx;
 
 import rx.functions.Func1;
-
+import rx.Observable;
 public class Functions {
 
     public static Func1<Integer, Boolean> isEven() {
@@ -16,4 +16,66 @@ public class Functions {
     private static boolean isEven(Integer integer) {
         return (integer % 2) == 0;
     }
+
+
+    public static Func1<Integer, Integer> twoTimes() {
+        return new Func1<Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer) {
+                return twoTimes(integer);
+            }
+        };
+    }
+
+    private static Integer twoTimes(Integer integer) {
+        return (integer * 2) ;
+    }
+
+    public static Func1<Integer, String> prependByInteger() {
+        return new Func1<Integer, String>() {
+            @Override
+            public String call(Integer integer) {
+                return prependByInteger(integer);
+            }
+        };
+    }
+
+    private static String prependByInteger(Integer integer) {
+        return ("Integer : " + integer) ;
+    }
+
+    public static Func1<Integer, Observable<Integer>> threeTimes() {
+        return new Func1<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> call(Integer integer) {
+                return threeTimes(integer);
+            }
+        };
+    }
+
+    private static Observable<Integer> threeTimes(Integer integer) {
+        return Observable.from(integer, integer, integer);
+    }
+
+    public static Func1<Integer, Observable<Integer>> threeTimesEven() {
+        return new Func1<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> call(Integer integer) {
+                return threeTimesEven(integer);
+            }
+        };
+    }
+
+    private static Observable<Integer> threeTimesEven(Integer integer) {
+        if ((integer % 2) == 0)
+        {
+            return Observable.from(integer, integer, integer);
+        }
+        else {
+            return Observable.from(integer);
+        }
+
+
+    }
+
 }
--- workshops/RxJava/src/main/java/com/novoda/workshop/rx/RxJavaBasics.java
+++ workshops/RxJava/src/main/java/com/novoda/workshop/rx/RxJavaBasics.java
@@ -5,9 +5,14 @@ import com.novoda.workshop.rx.observer.IntegerPrinterObserver;
 import java.util.Arrays;
 import java.util.List;
 
+import com.novoda.workshop.rx.observer.StringPrinterObserver;
 import rx.Observable;
 
 import static com.novoda.workshop.rx.Functions.isEven;
+import static com.novoda.workshop.rx.Functions.prependByInteger;
+import static com.novoda.workshop.rx.Functions.twoTimes;
+import static com.novoda.workshop.rx.Functions.threeTimes;
+import static com.novoda.workshop.rx.Functions.threeTimesEven;
 
 public class RxJavaBasics {
 
@@ -31,6 +36,26 @@ public class RxJavaBasics {
                 .filter(isEven())
                 .subscribe(new IntegerPrinterObserver());
 
+        //Multiply by 2
+        Observable.from(INTEGERS).map(twoTimes())
+                .subscribe(new IntegerPrinterObserver());
+
+        //Get only even element and multiply them by 2
+        Observable.from(INTEGERS).filter(isEven())
+                .map(twoTimes())
+                .subscribe(new IntegerPrinterObserver());
+
+        //Prepend the string "Integer : " in front of every element
+        Observable.from(INTEGERS).map(prependByInteger())
+                .subscribe(new StringPrinterObserver());
+
+        //Repeat 3 time every element
+        Observable.from(INTEGERS).flatMap(threeTimes()).subscribe(new IntegerPrinterObserver());
+
+        //Repeat 3 times every even element only say once odd ones
+        Observable.from(INTEGERS).flatMap(threeTimesEven()).subscribe(new IntegerPrinterObserver());
+
+        //Fail if an element is not even.
     }
 
 }
